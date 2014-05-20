a179d73dfb42edf4091b87c0ccd158fafb14b92a
Nick Reid <nickreid@nickreid.com>
2014-05-20 12:00:34 -0400

    added icons link to homepage

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
+    (r'^icons', TemplateView.as_view(template_name="homepage/icons.html")),
     (r'^', TemplateView.as_view(template_name="homepage/home.html")),
 )
